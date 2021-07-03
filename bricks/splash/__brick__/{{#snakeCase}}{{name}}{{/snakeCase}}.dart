760eacd0cdf335766e62035620849aff66528888
Martin Afani <martin@orderly.ai>
2021-07-03 12:51:16 +0200

    Update splash brick

--- bricks/splash/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}.dart
+++ bricks/splash/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+///
 class {{#pascalCase}}{{name}}{{/pascalCase}} extends StatelessWidget {
-  const {{#pascalCase}}{{name}}{{/pascalCase}} ({Key key}) : super(key: key);
+  ///
+  const {{#pascalCase}}{{name}}{{/pascalCase}} ({@required this.url, this.height=100, Key key}) : super(key: key);
+
+  ///
+  final String url;
+
+  ///
+  final int height;
 
   static Route get route => MaterialPageRoute(builder: (_) => const {{#pascalCase}}{{name}}{{/pascalCase}}());
 
   @override 
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: Text('Splash Screen'))
+  Widget build(BuildContext context) => Container(
+    child: Center(child: Image.asset(url, height: height))
   );
 }
