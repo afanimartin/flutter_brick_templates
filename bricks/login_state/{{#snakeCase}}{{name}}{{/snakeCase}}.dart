9de2979713e68ea6b74194abeb3635c455d3ac40
Martin Afani <martin@orderly.ai>
2021-09-04 21:39:37 +0200

    Add login state cubit

---
+++ bricks/login_state/{{#snakeCase}}{{name}}{{/snakeCase}}.dart
@@ -0,0 +1,9 @@
+class {{#pascalCase}}{{name}}{{/pascalCase}} extends Equatable {
+  ///
+  {{#pascalCase}}{{name}}{{/pascalCase}}();
+
+  @override 
+  List<Object> get props => [];
+}
+
+class InitialState extends {#pascalCase}}{{name}}{{/pascalCase}} {}
